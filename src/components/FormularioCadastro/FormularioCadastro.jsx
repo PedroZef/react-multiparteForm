454a25c3f9436a449d3250237b16bf76bc0ef2ba
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2023-08-29 15:59:34 -0300

    Utilizando o componente Stepper do MaterialUI nos formularios

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,30 +3,43 @@ import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
 import { useEffect } from "react";
+import { Step, StepLabel, Stepper, Typography } from "@material-ui/core";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
   const [dadosColetados, setDados] = useState({});
 
   useEffect(() => {
-    console.log(dadosColetados);
-  })
+    if (etapaAtual === formularios.length-1) {
+      aoEnviar(dadosColetados);
+    }
+
+  });
 
   const formularios = [
-    <DadosUsuario aoEnviar={ coletarDados} />,
-    <DadosPessoais aoEnviar={ coletarDados} validarCPF={validarCPF} />,
-    <DadosEntrega aoEnviar={ coletarDados} />,
+    <DadosUsuario aoEnviar={coletarDados} />,
+    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={coletarDados} />,
+    <Typography variant="hs">Obrigado peloCadastro!</Typography>
   ];
 
   function coletarDados(dados) {
-    setDados({...dadosColetados, ...dados});
-    
+    setDados({ ...dadosColetados, ...dados });
+
     proximo();
-  };
+  }
   function proximo(dados) {
     setEtapaAtual(etapaAtual + 1);
   }
-  return <>{formularios[etapaAtual]}</>;
+  return <>
+  <Stepper activeStep={etapaAtual}>
+    <Step><StepLabel>Login</StepLabel></Step>
+    <Step><StepLabel>Pessoal</StepLabel></Step>
+    <Step><StepLabel>Entrega</StepLabel></Step>
+    <Step><StepLabel>Finalização</StepLabel></Step>
+  </Stepper>
+  {formularios[etapaAtual]}
+  </>;
 }
 
 export default FormularioCadastro;
